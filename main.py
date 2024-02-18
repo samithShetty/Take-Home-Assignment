1ccdc8c2e19675b837703ec4de33936062520bef
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2024-02-18 10:40:17 -0500

    Implemented bounds-check utilities using shapely

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import utils
+from utils import get_clinician_status, parse_geojson, clinician_outside_zone
 
 NUM_EMPLOYEES = 6
 
 #TODO: Implement Time interval
 
 for i in range(1, NUM_EMPLOYEES+1):
-    geo_info = utils.get_clinician_status(i)
-    if utils.is_clinician_outside_zone(geo_info):
+    geo_info = get_clinician_status(i)
+    #TODO: Implement response validation
+    location, zone = parse_geojson(geo_info)
+    if clinician_outside_zone(location, zone):
         # TODO: Implement Email Sending
-        print("Alert! Clinician {i} is outside of their zone!") 
+        print("ALERT! Clinician {i} is outside of their zone!") 
+    else:
+        print("Clinician {i} is safely in their zone!") 
--- utils.py
+++ utils.py
@@ -1,38 +1,53 @@
 import requests
-import geojson
+from geojson import GeoJSON, loads
+from shapely import from_geojson
+from shapely.geometry import  Point, GeometryCollection
 
 BASE_URL = "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test"
 
-def get_clinician_status(clinician_id: int) -> geojson.GeoJSON:
+def get_clinician_status(clinician_id: int) -> GeoJSON:
     endpoint = f"{BASE_URL}/clinicianstatus/{clinician_id}"
     response = requests.get(endpoint)
-    info = geojson.loads(response.text)
+    info = loads(response.text)
     return info
 
-def get_clinician_location(geo_info: geojson.GeoJSON) -> list[float]:
-    # GeoJSON does not guarantee any order, so we should check all features
-    # The feature for clinician location will be the only 'Point'
-    for feature in geo_info['features']:
-        if feature['geometry']['type'] == 'Point':
-            return feature['geometry']['coordinates']
+def parse_geojson(geo_info: GeoJSON) -> tuple[Point, GeometryCollection]:
+    """
+    Convert GeoJSON into usable Shapely Geometry to perform bounds checks
+    """
+    zones = []
+    location = None
+    features = geo_info['features']
+    for feature in features:
+        shape = from_geojson(str(feature))
+        if shape.geom_type == 'Point':
+            location = shape
+        else:
+            zones.append(shape)
+    return location, GeometryCollection(zones)
 
-def get_bounding_zones(geo_info: geojson.GeoJSON) -> list[list[float]]:
-    # There will sometimes be multiple separate zones in the GeoJSON
-    # Each will be their own 'Polygon' feature, so we should return the list of all of them
-    polygons = []
-    for feature in geo_info['features']:
-        if feature['geometry']['type'] == 'Polygon':
-            polygons.append(feature['geometry']['coordinates'])
-    return polygons
+def clinician_outside_zone(location: Point, bounding_zone: GeometryCollection) -> bool:
+    return not location.intersects(bounding_zone)
 
-def is_clinician_outside_zone(geo_info: geojson.GeoJSON) -> bool:
-    location = get_clinician_location(geo_info)
-    bounding_zones = get_bounding_zones(geo_info)
-    for zone in bounding_zones:
-        if is_outside_zone(location, zone):
-            return True
-    return False
 
-def is_outside_zone(point, zone) -> bool:
-    #TODO: Implement Point-in-Polygon Collision
-    return False
+# UNUSED: My own custom implementation of point-in-polygon collision, using the Ray-Casting Algorithm
+# Replaced by the shapely library's intersects() method
+def is_outside_polygon(point: list[float], polygon: list[list[float]]) -> bool:
+    """
+    Cast horizontal ray (to simplify math) from point and check intersection with all lines of the polygon.
+    If intersection count is even -> point is outside, if odd -> point is inside 
+    """
+    x,y = point
+    is_outside = False
+    vert1_x, vert1_y = polygon[0]
+    for vertex in polygon[1:]:
+        vert2_x, vert2_y = vertex
+        if vert1_y == vert2_y:
+            if y == vert1_y and min(vert1_x,vert2_x) <= x <= max(vert1_x, vert2_x):
+                is_outside = not is_outside
+        else:        
+            x_intersection = vert1_x + (vert2_x - vert1_x) * (y-vert1_y) / (vert2_y-vert1_y)
+            if min(vert1_x,vert2_x) <= x_intersection <= max(vert1_x, vert2_x) and x_intersection <= x:
+                is_outside = not is_outside
+        vert1_x, vert1_y = vert2_x, vert2_y  
+    return is_outside
